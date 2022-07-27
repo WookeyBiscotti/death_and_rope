01213807507087587db241df117f322182d7c47d
Alexandr Savchenko <alexandr.savchenko@picsart.com>
2022-07-27 21:09:15 +0300

    i18n script update

---
+++ tools/test_file.cpp
@@ -0,0 +1,13 @@
+void testFn1(){
+    ALTR(EN, "Test string" " qwdaedawdaw "
+    "qwdawdawdawda ");
+}
+
+void testFn2(){
+    ALTR(EN, "Test string" " qwdaedawdaw "
+    "qwdawdawdawda ");
+}
+
+void testFn3(){
+    ALTR(RU, "ывфваывфвы" "йцуйцу");
+}
